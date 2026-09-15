949b12d598c610869da241edd1dc14a1869fc0f0
Mayank Srivastava <mayankshrivastava1624@gmail.com>
2026-09-15 14:07:32 +0530

    Initial commit

---
+++ src/main/java/sample/webmvc/controller/UserController.java
@@ -0,0 +1,28 @@
+package sample.webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/electronics")
+public class UserController {
+
+	@RequestMapping("/")
+	public String greet() {
+		System.out.println("UserController.greet()");
+		return "electronics";
+
+	}
+
+//	/WEB-INF/JSP/welcome.jsp
+
+//	@RequestMapping(value =  "/movies" ,method = RequestMethod.GET)
+	@GetMapping(value = { "/movies", "/films" })
+	public String movie() {
+		System.out.println("UserController.movie()");
+		return "movies";
+
+	}
+
+}
