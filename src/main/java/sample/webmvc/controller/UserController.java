80440e145c071bc5052ab5ee453c6702b5d2bbee
Mayank Srivastava <mayankshrivastava1624@gmail.com>
2026-09-19 20:28:25 +0530

    update

--- src/main/java/sample/webmvc/controller/UserController.java
+++ src/main/java/sample/webmvc/controller/UserController.java
@@ -3,6 +3,8 @@ package sample.webmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,13 +13,42 @@ public class UserController {
 
 	// @RequestParam can read Query Parameter
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String greet(@RequestParam(name="user", defaultValue = "GuestUser") String user, Model model) {
 		System.out.println("UserController.greet()");
 		
 		model.addAttribute("user", user);
 		
 		return "welcome";
+
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		System.out.println("UserController.login()");
+      return "login";
+
+	}
+	
+	
+	@GetMapping("/path/{id}")
+	public String pathVariable(@PathVariable(name = "id") int id) {
+		System.out.println("UserController.pathVariable: "+id);
+      return "welcome";
+
+	}
+	
+	
+	@PostMapping("/login")
+	public String userLogin(@RequestParam(name="username") String username, @RequestParam(name="password") String password, Model model) {
+		
+		System.out.println("UserController.userLogin : "+username);
+		System.out.println("UserController.userLogin : "+password);
+		
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		
+      return "profile";
 
 	}
 
