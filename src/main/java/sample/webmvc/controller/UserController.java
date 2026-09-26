ee2792fa3b70a94eac469785dab37971a06b258d
Mayank Srivastava <mayankshrivastava1624@gmail.com>
2026-09-26 13:17:52 +0530

    update

--- src/main/java/sample/webmvc/controller/UserController.java
+++ src/main/java/sample/webmvc/controller/UserController.java
@@ -1,46 +1,79 @@
 package sample.webmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import sample.webmvc.entity.User;
+import sample.webmvc.service.UserService;
 
 @Controller
 public class UserController {
 
-	// @RequestParam can read Query Parameter
+	@Autowired
+	 UserService userService;
 	
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+
 	@GetMapping("/")
-	public String greet(@RequestParam(name="user", defaultValue = "GuestUser") String user, Model model) {
-		System.out.println("UserController.greet()");
-		
-		model.addAttribute("user", user);
+	public String greet() {
+		System.out.println("UserController.greet : ");
 		
 		return "welcome";
 
 	}
 	
+	
 	@GetMapping("/login")
 	public String login() {
 		System.out.println("UserController.login()");
-      return "login";
+		return "login";
 
 	}
 	
 	
+	
 	@GetMapping("/path/{id}")
-	public String pathVariable(@PathVariable(name = "id") int id) {
-		System.out.println("UserController.pathVariable: "+id);
-      return "welcome";
+	public String pathVariablle(@PathVariable(name = "id") int id) {
+		System.out.println("UserController.pathVariablle : "+id);
+		return "welcome";
+	}
+	
+	@GetMapping("/sign-up")
+	public String signUp() {
+		System.out.println("UserController.login()");
+		return "signup";
+
+	}
+	
+	
+	@PostMapping("/sign-up")
+	public String saveUser(@RequestParam(name = "name") String name,@RequestParam(name = "gender") String gender,@RequestParam(name = "address") String address,Model model) {
+		
+		System.out.println("UserController.userLogin : "+name);
+		System.out.println("UserController.userLogin : "+gender);
+		
+		User user = new User(name, gender, address) ;
+		
+		userService.saveUser(user);
+		
+		model.addAttribute("user", user);
+		
+		return "success";
 
 	}
 	
 	
 	@PostMapping("/login")
-	public String userLogin(@RequestParam(name="username") String username, @RequestParam(name="password") String password, Model model) {
+	public String userLogin(@RequestParam(name = "username") String username,@RequestParam(name = "password") String password,Model model) {
 		
 		System.out.println("UserController.userLogin : "+username);
 		System.out.println("UserController.userLogin : "+password);
@@ -48,9 +81,10 @@ public class UserController {
 		model.addAttribute("username", username);
 		model.addAttribute("password", password);
 		
-      return "profile";
+		return "profile";
 
 	}
-
-
+	
+	
+	
 }
